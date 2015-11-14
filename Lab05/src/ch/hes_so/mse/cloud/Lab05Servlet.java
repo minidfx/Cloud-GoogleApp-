7191143974f30d0a92dd07920b4b181f63ac14dc
Burgy Benjamin <minidfx@gmail.com>
2015-11-14 10:37:29 +0100

    Create new one  that works!

--- Lab05/src/ch/hes_so/mse/cloud/Lab05Servlet.java
+++ Lab05/src/ch/hes_so/mse/cloud/Lab05Servlet.java
@@ -1,10 +1,10 @@
-package ch.hesso.mse.cloud;
+package ch.hes_so.mse.cloud;
 
 import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
-public class Lab5Servlet extends HttpServlet {
+public class Lab05Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
