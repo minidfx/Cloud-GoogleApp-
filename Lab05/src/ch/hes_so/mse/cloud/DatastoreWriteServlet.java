9642aaa8614d11127db000cee795e52f04559c7b
Burgy Benjamin <minidfx@gmail.com>
2015-11-14 14:08:14 +0100

    Add the link of the new datastore .console

--- Lab05/src/ch/hes_so/mse/cloud/DatastoreWriteServlet.java
+++ Lab05/src/ch/hes_so/mse/cloud/DatastoreWriteServlet.java
@@ -10,6 +10,13 @@ import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 
+/**
+ * 
+ * @url https://console.developers.google.com/datastore/ 
+ * 
+ * @author Burgy Benjamin
+ */
+
 @SuppressWarnings("serial")
 public class DatastoreWriteServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
