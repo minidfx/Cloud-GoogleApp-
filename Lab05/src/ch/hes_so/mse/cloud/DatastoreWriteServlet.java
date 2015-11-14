f558538f814edf9db99dbcdc513789f4b969b88b
Burgy Benjamin <minidfx@gmail.com>
2015-11-14 11:26:21 +0100

    Add a new servlet for writing into the database.

---
+++ Lab05/src/ch/hes_so/mse/cloud/DatastoreWriteServlet.java
@@ -0,0 +1,12 @@
+package ch.hes_so.mse.cloud;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class DatastoreWriteServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world from Datastore");
+	}
+}
